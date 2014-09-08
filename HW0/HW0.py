7846df593b0e778cac43c17d47d6acc2034afbce
coolis <wayne.liwen@gmail.com>
2014-09-08 13:14:47 -0400

    read input to matrix

--- HW0/HW0.py
+++ HW0/HW0.py
@@ -4,13 +4,16 @@ import sklearn
 import skimage
 from matplotlib import *
 from numpy import*
+import matplotlib.pyplot as plt
 import csv
 
-attributes = genfromtxt("/Users/WayneLI/Desktop/CS5785/HW0/iris.data", dtype=[('sepalL','f8'),('sepalW','f8'),
-('petalL','f8'), ('petalW','f8'), ('class','S15')], delimiter=',')
+attributes = genfromtxt("/Users/WayneLI/Desktop/CS5785/HW0/iris.data", usecols=(0,1,2,3) ,delimiter=',')
 
 xs = numpy.array([1, 2, 3, 4, 5, 6, 7]) 
 ys = numpy.array([3, 2, 5, 1, 3, 3, 2]) 
-colors = ["r","r","r","b","b","g","g"] 
-pyplot(xs, ys, c=colors)
+colors = ["r","b","g"] 
+plt.scatter(xs, ys, c=colors)
+plt.show()
+
+
 
