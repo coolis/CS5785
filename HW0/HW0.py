58c1f4ffe316e83372e15e53dfefa89186449c15
coolis <wayne.liwen@gmail.com>
2014-09-08 14:39:01 -0400

    Beta2 with plot graph

--- HW0/HW0.py
+++ HW0/HW0.py
@@ -7,27 +7,43 @@ from numpy import*
 import matplotlib.pyplot as plt
 import csv
 
+#initialize the colors for each species
+def initColors(species):
+    colors = []
+    for s in species:
+        if s == 'Iris-setosa':
+            colors.append('r')
+        elif s == 'Iris-versicolor':
+            colors.append('b')
+        elif s == 'Iris-virginica':
+            colors.append('g')
+    return colors
+
+#read the length and width of each sample and push to a N x p dimentional matrix
 attributes = genfromtxt("/Users/WayneLI/Desktop/CS5785/HW0/iris.data", usecols=(0,1,2,3) ,delimiter=',')
+#read the species and push to a p dimentional vector
 species = genfromtxt("/Users/WayneLI/Desktop/CS5785/HW0/iris.data", dtype="|S15", usecols=(4) ,delimiter=',')
 
-fig = plt.figure()
-colorR = ["r"]*50
-colorB = ["b"]*50
-colorG = ["g"]*50
-colors = numpy.concatenate((colorR, colorB, colorG))
+#create the plot panel
+fig = plt.figure()  
+#initialize the color for each species   
+colors = initColors(species)  
 
+#plot each attribute against the other
 for i in range(0, 4):
+    #x axis value
     xs = attributes[:,i]
     for j in range(0, 4):
         if i == j:
             continue
         else:
+            #y axis value
             ys = attributes[:,j]
+            #plot the graph on the plot panel
             ax = fig.add_subplot(4, 4, i*4+j+1)
             ax.scatter(xs, ys, c=colors)
-            
+
+#set the layout and show    
+fig.tight_layout()        
 plt.show()
-
-
-
 
